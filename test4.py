6141dbba1333e339410943863a5fe4c0574917ce
Rory Bresnahan <bresnahr@onid.oregonstate.edu>
2015-07-23 01:21:04 -0400

    Newest version 7/22/15, getting data from the database and putting in the grid

--- test4.py
+++ test4.py
@@ -1,13 +1,93 @@
-#!/usr/bin/python
+'''**************************************
+# Name: Nikolay Goncharenko, Rory Bresnahan
+# Email: goncharn@onid.oregonstate.edu
+# Class: CS419 - Capstone Project
+# Assignment: Python Ncurses UI for 
+# MySQL/PostgreSQL Database Management
+**************************************'''
+
 import npyscreen, curses
 import random
 import psycopg2
-import sys
+import psycopg2.extras
 
+# $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "goncharn-db", $myPassword, "goncharn-db");
 rowNumber = 5
-tableName = ''
+colTitles = (1,2,3,4)#for testing
+con_string = "dbname='muepyavy' user='muepyavy' host='babar.elephantsql.com' password='EQoh7fJJNxK-4ag4SNUIYwzzWqTVzj-8'"
+
+'''**************************************************
+* Class Database inherits object class
+*
+* Purpose:  Connect to the database and query it
+**************************************************'''
+class Database(object):
+	def list_all_tables(self):
+		try:
+			conn = psycopg2.connect("dbname='muepyavy' user='muepyavy' host='babar.elephantsql.com' password='EQoh7fJJNxK-4ag4SNUIYwzzWqTVzj-8'")
+		except:
+			print "I am unable to connect to the database"
+		
+		# fetch list of the tables in the database
+		cursor = conn.cursor()
+		cursor.execute("SELECT table_name FROM information_schema.tables \
+							WHERE \
+								table_type = 'BASE TABLE' AND table_schema = 'public' \
+							ORDER BY table_type, table_name")
+		tables = cursor.fetchall()
+		cursor.close()
+		return tables
+	
+
+	def add_record(self, last_name = '', other_names='', email_address=''):
+		db = sqlite3.connect(self.dbfilename)
+        	c = db.cursor()
+        	c.execute('INSERT INTO records(last_name, other_names, email_address) \
+                	    VALUES(?,?,?)', (last_name, other_names, email_address))
+        	db.commit()
+        	c.close()
+    
+	def update_record(self, record_id, last_name = '', other_names='', email_address=''):
+		db = sqlite3.connect(self.dbfilename)
+	        c = db.cursor()
+	        c.execute('UPDATE records set last_name=?, other_names=?, email_address=? \
+        	            WHERE record_internal_id=?', (last_name, other_names, email_address, \
+                                                        record_id))
+	        db.commit()
+        	c.close()    
+	def delete_record(self, record_id):
+        	db = sqlite3.connect(self.dbfilename)
+        	c = db.cursor()
+        	c.execute('DELETE FROM table_name where record_internal_id=?', (record_id,))
+        	db.commit()
+   		c.close()    
+#I was working on the following 2 methods: list_all_records, get_record   Will have to change to accept different tables   
+	def list_all_records(self, ):
+        	db = psycopg2.connect(con_string)
+	        c = db.cursor()
+        	c.execute('SELECT * from company')
+        	records = c.fetchall()
+        	c.close()
+        	return records
+    
+	def get_record(self):
+		db = psycopg2.connect(con_string)
+		cursor = db.cursor(cursor_factory=psycopg2.extras.DictCursor)#makes a dictionary object for iteration
+		work_mem = 2048
+		cursor.execute('SET work_mem to %s', (work_mem,))
+		cursor.execute('SELECT * from company')
+		memory = cursor.fetchone()
+        	cursor.close()
+		return memory
+	       
 
 
+'''**************************************************
+   Class MyGrid inherits GridColTitles class
+   
+   Purpose:  display data from database as greed,
+    visualazing a table in database
+**************************************************'''
 class MyGrid(npyscreen.GridColTitles):
     # You need to override custom_print_cell to manipulate how
     # a cell is printed. In this example we change the color of the
@@ -19,65 +99,85 @@ class MyGrid(npyscreen.GridColTitles):
            actual_cell.color = 'GOOD'
         else:
            actual_cell.color = 'DEFAULT'
-		   
-class theDatabase(object):
-	def __init__ (self):
+
+
+'''**************************************************
+   Class TableList inherits MultiLineAction class
+   
+   Purpose:  display list of tables as list and define an action
+   when one of the tables is selected 
+**************************************************'''
+class TableList(npyscreen.MultiLineAction):
+    def __init__(self, *args, **keywords):
+        super(TableList, self).__init__(*args, **keywords)
+
+    def display_value(self, value):
+        return "%s" % (value[0])
+    
+    def actionHighlighted(self, act_on_this, keypress):
+        #self.parent.parentApp.getForm('EDITRECORDFM').value = act_on_this[0]
+        #parent.parentApp.switchForm('EDITRECORDFM')
+		selectedTableName = act_on_this[0]
+		self.parent.parentApp.getForm('Menu').selectTable = selectedTableName
+		self.parent.parentApp.switchForm('Menu')
+
 		
-		con = None
-		try:
-			con = psycopg2.connect("dbname='template1' user='dbuser' host='localhost' password='dbpass'")
-		except psycopg2.DatabaseError, e:
-			if con:
-				con.rollback()
-			print 'Error %s' % e
-			sys.exit(1)
-		finally:
-			if con:
-				con.close()
+'''**************************************************
+   Class TableListDisplay inherits FormMutt class
+   
+   Purpose:  Container for displaying of the dynamic list
+**************************************************'''
+class TableListDisplay(npyscreen.FormMutt):
+	
+	# type of widget to be displayed
+	MAIN_WIDGET_CLASS = TableList
+	MAIN_WIDGET_CLASS_START_LINE = 2
+	STATUS_WIDGET_X_OFFSET = 5
+	
+	def beforeEditing(self):
+		self.update_list()
+	
+	def update_list(self):
+		self.wStatus1.value =  ' Select Table From List   '
+		self.wMain.values = self.parentApp.myDatabase.list_all_tables()
+		self.wMain.relx= 3
+		self.wMain.display()
 
-class selectTableForm(npyscreen.Form):
+
+'''**************************************************
+   Class TableMenuForm inherits ActionForm class
+   
+   Purpose:  Displays main table menu and grid
+**************************************************'''													
+class TableMenuForm(npyscreen.ActionForm):
+	# set screen redirection based on user choice
 	def afterEditing(self):
-		self.parentApp.getForm('Menu').value = self.table.get_selected_objects()
-		self.parentApp.setNextForm('Menu')
-	def create(self):
-		self.table = self.add(npyscreen.TitleSelectOne, max_height=3, name='Select Table',\
-						values = ['Table 1', 'Table 2', 'Table 3'],\
-						scroll_exit = True  # Let the user move out of the widget by pressing 
-						 		    # the down arrow instead of tab.  Try it without to see the difference
-				     )
-		tableName = self.table.value
-		#self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application
-		
-	def exit_application(self):
-		curses.beep()
-		self.parentApp.setNextForm(None)
-		self.editing = False
-
-
-class tableMenuForm(npyscreen.ActionForm):
-	def afterEditing(self):
-		#self.parentApp.setNextForm(None)
-		self.rowNum = self.add(npyscreen.TitleText, name='New R: ', value = self.action.get_selected_objects())
-		if self.action.get_selected_objects() == ['Add Row']:
-			self.rowNum = self.add(npyscreen.TitleText, name='New R: ', value = "Equalll")
-			#self.update(clear=True)
-#		if self.action.values[2] == 'Add Row':
-			self.parentApp.setNextForm('addForm')
-#			self.parentApp.setNextForm('addForm')
-		#else:
-		#	self.parentApp.setNextForm('addForm')
+		selection = self.action.get_selected_objects()[0]
+		if selection == 'Add Row':
+			self.parentApp.setNextForm('Add Row')
+		elif selection == 'Edit Row':
+			self.parentApp.setNextForm('Edit Row')
+		elif selection == 'Delete Row':
+			self.parentApp.setNextForm('Delete Row')
+		elif selection == 'Next Page':
+			self.parentApp.setNextForm('Next Page')
+		elif selection == 'Prev Page':
+			self.parentApp.setNextForm('Prev Page')
+		else:
+			self.parentApp.setNextForm(None)
 		#self.parentApp.setNextFormPrevious()
 	
+	# Create Widgets
 	def create(self):
-		self.value = None
+		self.selectTable = None
 		self.rowNum = self.add(npyscreen.TitleText, name='Rows: ', value = str(rowNumber))
-		#rowNumber = int(str(self.rowNum.value))
 		self.action = self.add(npyscreen.TitleSelectOne, max_height=5, name='Select Action',\
-						values = ['Next Page', 'Prev Page', 'Add Row', 'Edit Row', 'Delete Row'],\
-						scroll_exit = True, #exit_right = True # Let the user move out of the widget by pressing 													  		 # the down arrow instead of tab.  Try it without to see the difference.
-					)    	    		    # move one line down from  the previous form
-
-#		self.nextrely += 1
+					values = ['Next Page', 'Prev Page', 'Add Row', 'Edit Row', 'Delete Row'],\
+					scroll_exit = True #Let the user move out of the widget by pressing 																		# the down arrow instead of tab.  Try it without to see the difference.
+																		)
+		# move one line down from  the previous form
+		self.nextrely += 1
+		# Create MyGrid Widget object
 #		self.myGrid =  self.add(MyGrid, col_titles = ['1','2','3','4'])
 #		# populate the grid
 #		self.myGrid.values = []
@@ -91,53 +191,83 @@ class tableMenuForm(npyscreen.ActionForm):
 #			self.myGrid.values.append(row)
 	
 	def beforeEditing(self):
-		if self.value:
-			self.name = "Table id : %s" % self.value
-		else:
-			self.name = "New Record"
+		if self.selectTable:
+			self.name = "%s" % self.selectTable
+			theList = self.parentApp.myDatabase.list_all_records()
+			self.myGrid =  self.add(MyGrid, col_titles = ['1','2','3','4'])
+               		# populate the grid
+   	     		self.myGrid.values = []
+			myrow = []
+		
+			#get one record (row) and put that in the grid
+			theOne = self.parentApp.myDatabase.get_record()
+			self.myGrid.values.append(theOne)#[0]
+			
+
+			#the following are different tests for listing data from list_all_reccords
+			#currently left off at trying to figure out how to parse the tuple that is data comes in, which seems to be what get_record does
+			#I figured out get_record last, so the answer may lie there, or maybe it has to do with how the table was created?  not sure yet..
+
+			#test1
+#			for z in theList:
+#				myrow.append(theList[z])
+#			self.myGrid.values.append(myrow)	
+#
+			#test2
+#			for x in range(rowNumber):
+#				row = []
+#				for y in range(4):
+#					row.append(theList[y])
+#				self.myGrid.values.append(row)
+#	
+			#test3
+#   	        	for x in range(rowNumber):
+#                     		row = []
+#                        	for y in range(4):
+#                        		row.append(theList[x])
+#                        	self.myGrid.values.append(row)
+		#return record
+
+#		else:
+#			self.name = "New Record"
 			
 		#return self.action.value;
 		#self.how_exited_handers[npyscreen.wgwidget.EXITED_ESCAPE]  = self.exit_application
-	def on_ok(self, clear=True):
-		if self.action == 'Add Row':
-			self.parentApp.switchForm('addForm')	
-			#self.update(clear=True)
-			#update(clear=True)
-	
-	def on_cancel(self):
-		self.parentApp.switchForm('MAIN')
+		
+	def exit_application(self):
+		curses.beep()
+		self.parentApp.setNextForm(None)
+		self.editing = False
 
 
-#	def exit_application(self):
-#		curses.beep()
-#		self.parentApp.setNextForm(None)
-#		self.editing = False
-
-class EditTable(npyscreen.ActionForm):
+'''*********************************************************
+   Class AddRowForm inherits ActionForm class
+   
+   Purpose:  Reponsible for adding a new row to the given table
+*********************************************************'''
+class AddRowForm(npyscreen.ActionForm):
+	def afterEditing(self):
+		self.parentApp.setNextFormPrevious()
+	# It's just prototype, non-dynamic
 	def create(self):
 		self.value = None
-		self.myName = self.add(npyscreen.TitleText, name = 'First Row: ')
-		
+		self.wgLastName   = self.add(npyscreen.TitleText, name = "Last Name:",)
+		self.wgOtherNames = self.add(npyscreen.TitleText, name = "Other Names:")
+		self.wgEmail      = self.add(npyscreen.TitleText, name = "Email:")
 
-	def beforeEditing(self):
-		if self.value == None:
-			self.name = "New Record"
-#			record = self.parentApp.mySelTab.get_record(self.value)
-#			self.name = "My record name is now %s" % record[0]
-#			self.newFirstRow.value = record[1]
-#		else:
-#			self.name = "New Record"
-	def on_cancel(self):
-		self.parentApp.switchFormPrevious('Menu')
 
+'''**************************************************
+   Class MyApplication inherits NPSAppManaged class
+   
+   Purpose:  Manages  flow between application screens.
+	It's a main app environment
+**************************************************'''
 class MyApplication(npyscreen.NPSAppManaged):
     def onStart(self):
-		mySelTab = selectTableForm()
-		selTableForm = self.addForm('MAIN', selectTableForm, name='Select Table')
-		tabMenuForm = self.addForm('Menu', tableMenuForm, name='Table Menu')
-		addTableForm = self.addForm('addForm', EditTable, name = 'Change Table')
-
-	#	return selTableForm.table.value
+		self.myDatabase = Database()
+		selTableF = self.addForm('MAIN', TableListDisplay, name='Select Table')
+		tabMenuF = self.addForm('Menu', TableMenuForm, name='Table Menu')
+		addRowF = self.addForm('Add Row', AddRowForm, name='Add Row')
 
 if __name__ == '__main__':
     TestApp = MyApplication().run()
